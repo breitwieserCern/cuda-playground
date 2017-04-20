c39a4876c3de9f2051ee0f2dfea0f0367bcfbb1e
Lukas Breitwieser <lukas.johannes.breitwieser@cern.ch>
2017-04-20 14:37:29 +0000

    Add proof of principle to create new object inside kernel using unified memory
    Solution is to write custom memory manager that reserves a large chunk
    of unified memory upfront.

--- unified_memory/data_element_um.cu
+++ unified_memory/data_element_um.cu
@@ -26,47 +26,96 @@
  */
 
 // https://github.com/parallel-forall/code-samples/blob/master/posts/unified-memory/dataElem_um.cu
+// nvcc -std=c++11 -arch=sm_30 -o data_element_um data_element_um.cu &&
+// ./data_element_um
 
-#include <string.h>
+#include <iostream>
 #include <stdio.h>
+#include <string.h>
 
-struct DataElement
-{
-  char *name;
-  int value;
-  float* data;
+// worst memory manager ever
+//   no deletes, no synchronization, ...
+// pre allocate large unified memory block that can be used to create new
+// objects from inside a kernel
+__device__ __managed__ char kUnifiedMemoryBlock[1000];
+__device__ __managed__ int kMemoryPosition = 0;
+
+class Managed {
+ public:
+  __host__ __device__ void *operator new(size_t len) {
+    void *ptr;
+    kMemoryPosition += len;
+    ptr = static_cast<void *>(kUnifiedMemoryBlock + kMemoryPosition);
+    return ptr;
+  }
+
+  __host__ __device__ void operator delete(void *ptr) {
+    // FIXME delete logic
+  }
 };
 
-__global__
-void Kernel(DataElement *elem) {
-  printf("On device: name=%s, value=%d, threadidx=%d\n", elem->name, elem->value, threadIdx.x);
+struct Foo : public Managed {
+  __host__ __device__ Foo() : data(123) {}
+  int data;
+};
+
+struct DataElement {
+  char *name;
+  int value;
+  float *data;
+  Foo *foos;
+  Foo *another_foo = nullptr;
+};
+
+__global__ void Kernel(DataElement *elem) {
+  printf("On device: name=%s, value=%d, threadidx=%d\n", elem->name,
+         elem->value, threadIdx.x);
 
   elem->name[0] = 'd';
   elem->value++;
-  elem->data[3+threadIdx.x] = 42;
+  elem->data[3 + threadIdx.x] = 42;
+  elem->foos[3 + threadIdx.x] = Foo();
+
+  // the following new reuqires the custom memory manager and Foo inheriting
+  // from Managed
+  // otherwise it would create a device ptr which cannot be accessed on the host
+  // -   -> segfault
+  //     This would lead to a mix of unified ptrs (name, data, foos)
+  //     and device only ptr (another_foo)
+  elem->another_foo = new Foo();
+  elem->another_foo->data = 321;
 }
 
 void launch(DataElement *elem) {
-  Kernel<<< 1, 2 >>>(elem);
+  Kernel<<<1, 2>>>(elem);
   cudaDeviceSynchronize();
 }
 
-int main(void)
-{
+int main(void) {
   DataElement *e;
-  cudaMallocManaged((void**)&e, sizeof(DataElement));
+  cudaMallocManaged((void **)&e, sizeof(DataElement));
 
   e->value = 10;
-  cudaMallocManaged((void**)&(e->name), sizeof(char) * (strlen("hello") + 1) );
+  cudaMallocManaged((void **)&(e->name), sizeof(char) * (strlen("hello") + 1));
   strcpy(e->name, "hello");
-  cudaMallocManaged((void**)&(e->data), sizeof(float) * 20 );
+  cudaMallocManaged((void **)&(e->data), sizeof(float) * 20);
+  cudaMallocManaged((void **)&(e->foos), sizeof(Foo) * 20);
 
   launch(e);
 
-  printf("On host: name=%s, value=%d, data[3]=%f, data[4]=%f\n", e->name, e->value, e->data[3], e->data[4]);
+  printf(
+      "On host: name=%s, value=%d, data[3]=%f, data[4]=%f, foos[3].data=%d\n",
+      e->name, e->value, e->data[3], e->data[4], e->foos[3].data);
+  printf("On host: another_foo.data=%d\n", e->another_foo->data);
+
+  // demonstrate that creating Foo on the host heap works the same way
+  auto host_foo = new Foo();
+  std::cout << "host_foo->data=" << host_foo->data << std::endl;
+  delete host_foo;
 
   cudaFree(e->name);
   cudaFree(e->data);
+  cudaFree(e->foos);
   cudaFree(e);
 
   cudaDeviceReset();
